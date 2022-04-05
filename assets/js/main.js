1b1b5666ac30f99d396c2999f044fbda6ed0b44c
Mohammed Abed <abedmohammed353@gmail.com>
2022-04-05 19:00:30 -0400

    Update V1.04

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,14 +63,16 @@ allSections.forEach(function (section) {
 const skillsContent = document.getElementsByClassName('skills__content')
 const skillsHeader = document.querySelectorAll('.skills__header')
 
+for(let i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+}
+
 function toggleSkills(){
     let itemClass = this.parentNode.className
-
-    for(let i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
     if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
+        this.parentNode.className = 'skills__content skills__open show-content'
+    } else{
+        this.parentNode.className = 'skills__content skills__close'
     }
 }
 
